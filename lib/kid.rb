c54e2cc60a06fc730ba3901650e3e34eff90e985
Bernard  Mel <bernard.mamuyac@gmail.com>
2020-04-03 18:29:00 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -3,11 +3,11 @@ require_relative './class_methods_module.rb'
 
 class Kid
   include Dance
+  include FancyDance::InstanceMethods
+
 
   extend MetaDancing
-
   extend FancyDance::ClassMethods
-include FancyDance::InstanceMethods
 
   attr_accessor :name
 
